68889bf6e77e7461c7affcc2d251f11728a91212
Tae-Yun Jung <jty6063@gmail.com>
2021-04-23 16:46:09 +0900

    new files

---
+++ RNN_main.m
@@ -0,0 +1,90 @@
+%% Network train
+clear;
+
+load RNN_training_data_set_AWGN_fft512_64  % trainig set load
+
+YTrain = categorical(YTrain,[1 0],{'ON','OFF'});
+
+warning off parallel:gpu:device:DeviceLibsNeedsRecompiling
+try
+    gpuArray.eye(2)^2;
+catch ME
+end
+try
+    nnet.internal.cnngpu.reluForward(1);
+catch ME
+end
+width = 16;
+
+layers = [ ...
+    sequenceInputLayer(2*width) % observation frequency width x 2
+        
+    lstmLayer(256,'OutputMode','last')
+    fullyConnectedLayer(32)
+    fullyConnectedLayer(2)
+    softmaxLayer
+    classificationLayer
+    ];
+
+options = trainingOptions('sgdm', ...
+    'ExecutionEnvironment','auto',...
+    'LearnRateSchedule','piecewise', ...
+    'InitialLearnRate',0.001, ...
+    'shuffle','every-epoch', ...
+    'MaxEpochs',10, ...
+    'MiniBatchSize', 31*20, ...
+    'plots','training-progress', ...
+    'Verbose',false);
+
+net = trainNetwork(XTrain,YTrain,layers,options);
+
+clear XTrain YTrain;
+save RNN_network_timeblock_64 net;
+
+%% TEST
+load RNN_test_data_set_AWGN_fft512_64  % test set load
+
+YTest = categorical(YTest,[1 0],{'ON','OFF'});
+
+FDR = zeros(length(YTest(1,:)),1); MDR = zeros(length(YTest(1,:)),1); ACC = zeros(length(YTest(1,:)),1);
+total_ON_count = 0;
+total_OFF_count = 0;
+for loop=1:length(YTest(1,:))
+    temp = classify(net,XTest(:,:,:,:,loop));
+    
+    for n=1:length(temp)
+        if YTest(n,loop) == 'ON'
+            if temp(n) ~= YTest(n,loop)
+                FDR(loop) = FDR(loop) + 1;
+            end
+            total_ON_count = total_ON_count + 1;
+        else
+            if temp(n) ~= YTest(n,loop)
+                MDR(loop) = MDR(loop) + 1;
+            end
+            total_OFF_count = total_OFF_count + 1;
+        end
+    end
+    FDR(loop) = FDR(loop)/total_ON_count;
+    MDR(loop) = MDR(loop)/total_OFF_count;
+    ACC(loop) = 1 - sum(temp~=YTest(:,loop))/length(temp);
+end
+
+SNR = -20:2:6;
+save RNN_performance_64block MDR FDR ACC SNR
+
+figure(1); 
+s1 = semilogy(SNR,MDR,'bo--','LineWidth',2); hold on;
+s2 = semilogy(SNR,FDR,'bo-','LineWidth',2); hold on;
+grid on;
+axis([-20 6 1e-4 1]); xticks(SNR);
+title('Miss & False Detection Ratio', 'fontsize', 12);
+xlabel('SNR (dB)'); ylabel('Probability');
+legend([s1, s2], 'Miss detection ratio', 'False detection ratio');
+
+figure(2); 
+plot(SNR,ACC,'bo-','LineWidth',2);
+grid on;
+axis([-20 6 0.55 1]);
+xlabel('SNR (dB)'); ylabel('Total Accuracy');
+
---
+++ RNN_signal_gen.m
@@ -0,0 +1,226 @@
+clear;
+%%%%%%%%%%%%%%%
+%%% Design Parameters %%
+%%%%%%%%%%%%%%%
+
+Fs = 16; % sampling frequency in MHz
+BW =0.5; % bandwidth in MHz
+
+N_fft = 512; % FFT size
+L_overlap = N_fft/2;   
+N_time_block = 64; % Observation time
+
+N_training = 1;
+N_test = 1;
+
+roll_off = 0.22;
+over_s1 = 4; % oversampling ratio for PSF
+over_s2 = 2; % oversampling ratio for 1st interpolation
+over_s3 = 5; % oversampling ratio for 2nd interpolation
+over_s = over_s1*over_s2*over_s3; % total oversampling ratio
+
+L_time_total = (N_fft-L_overlap)*(N_time_block-1)+N_fft;   
+N_sym = ceil(L_time_total/over_s)+1;    
+
+psf = rcosdesign(roll_off,30,over_s1,'normal');  
+intp_flt1 = rcosdesign(0.5,20,over_s2*2,'normal');
+intp_flt2 = rcosdesign(0.8,20,over_s3*2,'normal');
+psf = psf/sum(psf);
+intp_flt1 = intp_flt1/sum(intp_flt1);
+intp_flt2 = intp_flt2/sum(intp_flt2);
+
+mod_idx = 4; % 4:QPSK, 32:32QAM, 64:64QAM
+
+% candidates of carrier frequencies
+fc_table = -Fs/2+BW:BW:+Fs/2-BW;
+fc_table = fc_table + max(fc_table);
+
+width = round(N_fft/(Fs/BW));
+N_ch = length(fc_table);
+
+%%%%%%%%%%%%%%%%%%%
+%% Training data set generation %%
+%%%%%%%%%%%%%%%%%%%
+
+XTrain = zeros(2*width, N_time_block, 1, N_training*N_ch);
+YTrain = zeros(N_training*N_ch, 1);
+
+for loop=1:N_training
+    
+    SNR = rand(1)*70-20; % SNR -20~50dB random
+    
+    ch_status = randi([0 1],length(fc_table),1);
+    YTrain(N_ch*(loop-1)+1:N_ch*loop) = ch_status;
+    clear tx_sig
+    for n=1:length(fc_table)
+        if ch_status(n) == 1
+            data_t = randi([0 mod_idx-1], N_sym, 1);
+            data = [data_t; data_t; data_t];
+             SYM_t = qammod(data, mod_idx)*sqrt(0.5);
+            % Pulse shaping filter
+            temp = upfirdn(SYM_t,psf,over_s1,1)*sqrt(over_s1);
+            psf_out = temp((length(psf)+1)/2:end-(length(psf)-1)/2);
+            % interpolation (x2)
+            temp = upfirdn(psf_out,intp_flt1,over_s2,1)*sqrt(over_s2);
+            intp_out1 = temp((length(intp_flt1)+1)/2:end-(length(intp_flt1)-1)/2);
+            % interpolation (x5)
+            temp = upfirdn(intp_out1,intp_flt2,over_s3,1)*sqrt(over_s3);
+            intp_out2 = temp((length(intp_flt2)+1)/2:end-(length(intp_flt2)-1)/2);
+            % extract center block
+            if exist('tx_sig','var')
+                block = intp_out2(N_sym*over_s+1:2*N_sym*over_s);
+                tx_sig = tx_sig + block.*exp(1j*2*pi*fc_table(n)/Fs*[1:length(block)]');
+            else
+                block = intp_out2(N_sym*over_s+1:2*N_sym*over_s);
+                tx_sig = block.*exp(1j*2*pi*fc_table(n)/Fs*[1:length(block)]');
+            end
+        end
+    end
+    
+    % noise generation
+    if exist('tx_sig','var')
+        noise = sqrt(0.5)*crandn(size(tx_sig));
+    else
+        noise = sqrt(0.5)*crandn(L_time_total,1);
+    end
+    % rx signal + noise
+    rx_sig = sqrt(10^(SNR/10))*tx_sig + noise;
+    
+    % rx signal scalining
+    rx_sig = rx_sig/300;
+    
+    % rx signal vector to matrix
+    rx_sig_mat = zeros(N_fft,N_time_block);
+    idx = 1;
+    for n=1:N_time_block
+        temp = rx_sig(idx:idx+N_fft-1).*hanning(N_fft);
+        temp = abs(fft(temp,N_fft)/sqrt(N_fft));
+        temp = wshift('1D',temp,-round(N_fft/(Fs/BW)/2));
+        rx_sig_mat(:,n) = temp/max(temp);
+        idx = idx + N_fft - L_overlap;
+    end
+    
+    for n=1:N_ch
+        XTrain(:,:,1,(loop-1)*N_ch+n) = [rx_sig_mat(width*(n-1)+1:width*n,:); rx_sig_mat(end-width+1:end,:)];
+    end
+    
+    %     figure(1); hold off;
+    %     imshow(rx_sig_mat); disp(ch_status');
+    %     figure(2); hold off;
+    %     imshow(rx_sig_mat(width+1:width*2,:));
+    
+    if mod(loop,1000) == 0
+        disp(loop);
+    end
+    
+end
+rand_idx = randperm(N_training*N_ch);
+XTrain_matrix = XTrain(:,:,1,rand_idx); % shuffle
+XTrain = cell(length(XTrain_matrix(1,1,1,:)),1); % cell, used for RNN
+
+for n=1:length(XTrain_matrix(1,1,1,:))
+    XTrain{n} = XTrain_matrix(:,:,1,n);
+end
+YTrain = YTrain(rand_idx);
+
+im_h = width*2;
+im_w = N_time_block;
+
+S = sprintf('RNN_training_data_set_AWGN_fft512_%d.mat', N_time_block);
+save(S, 'XTrain', 'YTrain', 'im_h', 'im_w', '-v7.3');
+
+% figure(1)
+% imshow(rx_sig_mat);
+% figure(2)
+% imshow(XTrain{1});
+
+%%%%%%%%%%%%%%%%%
+%% Test data set generation %%
+%%%%%%%%%%%%%%%%%
+SNR_table = [-20:2:6];
+
+XTest_matrix = zeros(2*width,N_time_block,1,N_test*N_ch,length(SNR_table)); % matrix, used for RNN
+XTest = cell(N_test*N_ch,length(SNR_table)); % cell, used for RNN
+YTest = zeros(N_test*N_ch,length(SNR_table));
+
+for SNR_loop=1:length(SNR_table)
+    
+    SNR = SNR_table(SNR_loop);
+    
+    for loop=1:N_test
+        
+        ch_status = randi([0 1],length(fc_table),1);
+        YTest(N_ch*(loop-1)+1:N_ch*loop, SNR_loop) = ch_status;
+        clear tx_sig
+        for n=1:length(fc_table)
+            if ch_status(n) == 1
+                data_t = randi([0 mod_idx-1], N_sym, 1);
+                data = [data_t; data_t; data_t];
+                SYM_t = qammod(data, mod_idx)*sqrt(0.5);
+                % Pulse shaping filter
+                temp = upfirdn(SYM_t,psf,over_s1,1)*sqrt(over_s1);
+                psf_out = temp((length(psf)+1)/2:end-(length(psf)-1)/2);
+                % interpolation (x2)
+                temp = upfirdn(psf_out,intp_flt1,over_s2,1)*sqrt(over_s2);
+                intp_out1 = temp((length(intp_flt1)+1)/2:end-(length(intp_flt1)-1)/2);
+                % interpolation (x5)
+                temp = upfirdn(intp_out1,intp_flt2,over_s3,1)*sqrt(over_s3);
+                intp_out2 = temp((length(intp_flt2)+1)/2:end-(length(intp_flt2)-1)/2);
+                % extract center block
+                if exist('tx_sig','var')
+                    block = intp_out2(N_sym*over_s+1:2*N_sym*over_s);
+                    tx_sig = tx_sig + block.*exp(1j*2*pi*fc_table(n)/Fs*[1:length(block)]');
+                else
+                    block = intp_out2(N_sym*over_s+1:2*N_sym*over_s);
+                    tx_sig = block.*exp(1j*2*pi*fc_table(n)/Fs*[1:length(block)]');
+                end
+            end
+        end
+        
+        % noise generation
+        if exist('tx_sig','var')
+            noise = sqrt(0.5)*crandn(size(tx_sig));
+        else
+            noise = sqrt(0.5)*crandn(L_time_total,1);
+        end
+        % rx signal + noise
+        rx_sig = sqrt(10^(SNR/10))*tx_sig + noise;
+        
+        % rx signal scalining
+        rx_sig = rx_sig/300;
+        
+        % rx signal vector to matrix
+        rx_sig_mat = zeros(N_fft,N_time_block);
+        idx = 1;
+        for n=1:N_time_block
+            temp = rx_sig(idx:idx+N_fft-1).*hanning(N_fft);
+            temp = abs(fft(temp,N_fft)/sqrt(N_fft));
+            temp = wshift('1D',temp,-round(N_fft/(Fs/BW)/2));
+            rx_sig_mat(:,n) = temp/max(temp);
+            idx = idx + N_fft - L_overlap;
+        end
+        
+        for n=1:N_ch
+            XTest_matrix(:,:,1,(loop-1)*N_ch+n,SNR_loop) = [rx_sig_mat(width*(n-1)+1:width*n,:); rx_sig_mat(end-width+1:end,:)];
+        end
+        
+        for n=1:length(XTest_matrix(1,1,1,:,1))
+            XTest{n,SNR_loop} = XTest_matrix(:,:,1,n,SNR_loop);
+        end
+        
+        if mod(loop,1000) == 0
+            disp(loop);
+        end
+    end
+end
+
+S = sprintf('RNN_test_data_set_AWGN_fft512_%d.mat', N_time_block);
+save(S, 'XTest', 'YTest', '-v7.3');
+
+% figure(1)
+% imshow(rx_sig_mat);
+% figure(2)
+% imshow(XTest{1});
+
+
+
